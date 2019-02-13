5b33119dabb56461bcd427a788cadcbb87bb1c73
rmin92 <ray_min@outlook.com>
2019-02-13 18:46:55 -0500

    adding hello world to sandbox repo

---
+++ hello world.py
@@ -0,0 +1,9 @@
+# Hello World
+
+# I want this to be my first Python Project. Just a simple "Hello world"
+# This will motivate me when I look back at the more strenuous projects I have.
+#...
+
+
+print("hello world")
+print("My Name Is Ray")
