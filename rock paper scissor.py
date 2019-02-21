57f643bd185d7eec11da1f7e4b5e730eaef97c51
rmin92 <ray_min@outlook.com>
2019-02-21 17:34:09 -0500

    RPS code added

---
+++ rock paper scissor.py
@@ -0,0 +1,17 @@
+# Rock Paper Scissor
+
+import random
+import sys
+#begin the game and then loop after the first play
+
+def play():
+    while True:
+        p_choice + input ("Choose your weapon")
+        cpu_random = random.randint(1,3)
+        cpu_choice + cpu_random
+        if cpu_random == 1:
+            cpu_choice = "rock"
+        elif cpu_random == 2:
+            cpu_choice = "paper"
+        elif cpu_choice == 3:
+            cpu_choice = "scissors"
